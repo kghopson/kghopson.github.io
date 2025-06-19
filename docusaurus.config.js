5572e4b3413363aec92499b64b4892b205aac4a8
kghopson <kghopson@gmail.com>
2025-06-19 12:30:29 -0500

    pic should be bigger

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Your all-purpose technical writer',
   tagline: 'Versatility in words, mastery in lots of stuff',
-  favicon: 'img/me-circle-vine.png',
+  favicon: 'img/bigger-me-circle-vine.png',
 
   // Set the production url of your site here
   url: 'https://kghopson.github.io',
@@ -64,7 +64,7 @@ const config = {
         title: 'kghopson world',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/me-circle-vine.png',
+          src: 'img/bigger-me-circle-vine.png',
         },
         items: [
           {to: '/docs/resume', label: 'Resume', position: 'left'},
